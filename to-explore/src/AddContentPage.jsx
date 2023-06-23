419a717aa85e24fb36d4cf3738788135930d9877
elperez365 <ceti.vasco@gmail.com>
2023-06-23 18:04:08 +0200

    initial commit

---
+++ to-explore/src/AddContentPage.jsx
@@ -0,0 +1,25 @@
+function AddContentPage() {
+  return (
+    <div>
+      <div>
+        <button>torna indietro</button>
+      </div>
+      <div>
+        <input type="file" />
+      </div>
+      <div>
+        <label htmlFor="">Descrizione</label>
+        <input type="text" />
+      </div>
+      <div>
+        <h1>posizione</h1>
+        <img src="/src/Screenshot 2023-06-19 090430.png" alt="" />
+      </div>
+      <div>
+        <button>PUBBLICA</button>
+      </div>
+    </div>
+  );
+}
+
+export default AddContentPage;
--- to-explore/src/App.js
+++ to-explore/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
+import AddContentPage from "./AddContentPage";
 
-
-class App extends React.Component() {
-  render() {
-    return(
-      <div>
-        
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>
+      <AddContentPage />
+    </div>
+  );
 }
 
 export default App;
