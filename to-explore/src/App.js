9912f8bacbde186ffb791128c82e083cae412208
Andre <aanuta9997@gmail.com>
2023-06-29 15:01:51 +0200

    cleanded app component and added some components for the profile

--- to-explore/src/App.js
+++ to-explore/src/App.js
@@ -1,12 +1,9 @@
-import { Homepage } from "./Homepage";
-import { Profilo } from "./Profilo";
 import './index.css'
 
 export function App() {
   return(
     <div>
-      <Profilo />
-      {/* <Homepage /> */}
+      
     </div>
   )
 }
