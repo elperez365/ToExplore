1db80fd9a690706ec85c803ccb1733f1753dcc17
elperez365 <ceti.vasco@gmail.com>
2023-07-11 19:46:40 +0200

    .

--- to-explore/server/posts.mjs
+++ to-explore/server/posts.mjs
@@ -10,66 +10,6 @@ postsRouter.get(`/`, (req, res) => {
   res.json(posts.reverse());
 });
 
-postsRouter.post(`/post`, (req, res) => {
-  const id = uuid();
-  const comments = [];
-  const {
-    location,
-    region,
-    postData,
-    postUser,
-    postAvatar,
-    avatarColor,
-    img,
-    description,
-  } = req.body;
-
-  if (
-    location &&
-    region &&
-    postData &&
-    postUser &&
-    postAvatar &&
-    avatarColor &&
-    img
-  ) {
-    posts.push({
-      id,
-      location,
-      region,
-      postData,
-      postUser,
-      postAvatar,
-      avatarColor,
-      img,
-      description,
-      comments,
-    });
-    res
-      .status(200)
-      .json({ success: true, text: "il tuo post è stato pubblicato" });
-    writeFileSync(
-      "./pubblications.mjs",
-      `export const pubblications= ${JSON.stringify(posts)}`
-    );
-  } else
-    res.status(500).json({
-      success: false,
-      text: "qualcosa non va, controlla i dati inseriti",
-    });
-});
-// ESEMPIO BODY DEL POST CHE ARRIVA
-//   {
-//     "location": "comune del post",
-//     "region": "regione del post",
-//     "postData": "data del post",
-//     "postUser": "username di chi ha postato",
-//     "postAvatar": "AA",
-//     "avatarColor": "red",
-//     "img": "boh",
-//     "description": "descrizione del post",
-//   },
-
 postsRouter.get(`/region/:region`, (req, res) => {
   const { region } = req.params;
   const filteredByRegion = posts.filter((post) => post.region === region);
