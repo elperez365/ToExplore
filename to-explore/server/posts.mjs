aab3974f5b69f843a47f9b53e98af3974d8c10c2
elperez365 <ceti.vasco@gmail.com>
2023-07-15 17:31:45 +0200

    aggiunta la delete per i post

--- to-explore/server/posts.mjs
+++ to-explore/server/posts.mjs
@@ -1,5 +1,6 @@
 import Express from "express";
 import { pubblications } from "./pubblications.mjs";
+import { writeFileSync } from "fs";
 
 export const postsRouter = Express.Router();
 const posts = [...pubblications];
@@ -18,4 +19,26 @@ postsRouter.get(`/username/:username`, (req, res) => {
   const { username } = req.params;
   const filteredByusername = posts.filter((post) => post.postUser === username);
   res.json(filteredByusername);
+});
+
+postsRouter.delete(`/delete`, (req, res) => {
+  const { id } = req.body;
+  const postFound = posts.findIndex((el) => el.id === id);
+  if (postFound >= 0) {
+    const newPosts = posts.filter((el) => el.id !== id);
+
+    res
+      .status(200)
+      .json({ success: true, text: "il tuo post è stato cancellato" });
+
+    writeFileSync(
+      "./pubblications.mjs",
+      `export const pubblications= ${JSON.stringify(newPosts.reverse())}`
+    );
+  } else {
+    res.status(500).json({
+      success: false,
+      text: "il post da eliminare non esiste",
+    });
+  }
 });
