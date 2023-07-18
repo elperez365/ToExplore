42816314d3d482635afeedc9814d25ec7d1aad2b
elperez365 <ceti.vasco@gmail.com>
2023-07-18 16:16:47 +0200

    select linga sistemata

--- to-explore/src/Settings.jsx
+++ to-explore/src/Settings.jsx
@@ -4,7 +4,7 @@ import { Navbar } from "./Navbar";
 import { useContext } from "react";
 // import { useEffect } from "react";
 import LanguageContext from "./LanguageContext";
-import { Select } from "@mui/material";
+// import { Select } from "@mui/material";
 
 export function Settings({ setUserlogged }) {
   const { languageApp, SetLanguageApp } = useContext(LanguageContext);
@@ -28,23 +28,25 @@ export function Settings({ setUserlogged }) {
         <ButtonPrev />
       </Link>
       <div className="flex flex-col h-4/5 justify-around items-center">
-        <label
-          className="justify-center text-3xl flex flex-wrap w-full
+        <div className="flex flex-col justify-center items-center gap-2">
+          <label
+            className="justify-center text-3xl flex flex-wrap w-full
                 md:text-3xl gap-3"
-          for="language"
-        >
-          {languages.selectLanguages}
-        </label>
-        <Select
-          value={languageApp}
-          onChange={selectLanguage}
-          /* className="bg-gray-50 border border-gray-300 text-gray-900 text-3xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-36 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" */
-          id="language"
-          name="language"
-        >
-          <option value="it">Italiano</option>
-          <option value="en">English</option>
-        </Select>
+            for="language"
+          >
+            {languages.selectLanguages}
+          </label>
+          <select
+            value={languageApp}
+            onChange={selectLanguage}
+            className="bg-primary border border-gray-300 text-gray-900 text-3xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-48 p-2.5 "
+            id="language"
+            // name="language"
+          >
+            <option value="it">Italiano</option>
+            <option value="en">English</option>
+          </select>
+        </div>
 
         <Link to="/changeAvatarColor" className="md:w-full">
           <p
