aa44fcb566d76f3d4c4df996b043276b47cbb86f
elperez365 <ceti.vasco@gmail.com>
2023-07-18 12:15:46 +0200

    silentLogin

--- to-explore/src/SilentLogin.jsx
+++ to-explore/src/SilentLogin.jsx
@@ -1,11 +1,11 @@
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+
 import userLoggedContest from "./UserLoggedContest";
 import { Homepage } from "./Homepage";
 
 function SilentLogin({ component }) {
   const { logged } = useContext(userLoggedContest);
-  const navigate = useNavigate;
+
   if (logged) {
     return <Homepage />;
   } else return component;
