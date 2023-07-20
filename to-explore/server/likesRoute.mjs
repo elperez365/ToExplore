6141491e3b2ab5e3ef9923fc893dfc9fc5995ac2
elperez365 <ceti.vasco@gmail.com>
2023-07-20 12:32:37 +0200

    creata api userLikeIt

--- to-explore/server/likesRoute.mjs
+++ to-explore/server/likesRoute.mjs
@@ -7,73 +7,84 @@ export const likesRouter = Express.Router();
 const likes = { ...likesDB };
 
 likesRouter.post("/post", (req, res) => {
-    const { username, postId } = req.body;
+  const { username, postId } = req.body;
 
-    if (likes[username]) {
-        likes[username].push(postId);
-    } else {
-        likes[username] = [];
-        likes[username].push(postId);
-    }
+  if (likes[username]) {
+    likes[username].push(postId);
+  } else {
+    likes[username] = [];
+    likes[username].push(postId);
+  }
 
-    res.status(200).json({ success: true, text: "il like è stato aggiunto" });
-    writeFileSync(
-        "./likes.mjs",
-        `export const likesDB = ${JSON.stringify(likes)}`
-    );
+  res.status(200).json({ success: true, text: "il like è stato aggiunto" });
+  writeFileSync(
+    "./likes.mjs",
+    `export const likesDB = ${JSON.stringify(likes)}`
+  );
 });
 
 likesRouter.delete("/delete", (req, res) => {
-    const { username, postId } = req.body;
+  const { username, postId } = req.body;
 
-    if (likes[username]) {
-        likes[username] = likes[username].filter((el) => el !== postId);
+  if (likes[username]) {
+    likes[username] = likes[username].filter((el) => el !== postId);
 
-        res.status(200).json({
-            success: true,
-            text: "il like è stato eliminato",
-        });
-        writeFileSync(
-            "./likes.mjs",
-            `export const likesDB = ${JSON.stringify(likes)}`
-        );
-    } else {
-        res.status(500).json({
-            success: false,
-            text: "lo username non esiste",
-        });
-    }
+    res.status(200).json({
+      success: true,
+      text: "il like è stato eliminato",
+    });
+    writeFileSync(
+      "./likes.mjs",
+      `export const likesDB = ${JSON.stringify(likes)}`
+    );
+  } else {
+    res.status(500).json({
+      success: false,
+      text: "lo username non esiste",
+    });
+  }
 });
 
 likesRouter.post("/getLike", (req, res) => {
-    const { username, postId } = req.body;
+  const { username, postId } = req.body;
 
-    const foundLikedPost = likes[username].find((el) => el === postId);
+  const foundLikedPost = likes[username].find((el) => el === postId);
 
-    if (foundLikedPost) {
-        res.status(200).json({
-            success: true,
-            text: "il like è presente",
-        });
-    } else {
-        res.status(200).json({
-            succes: false,
-            text: "il like non è presente",
-        });
-    }
+  if (foundLikedPost) {
+    res.status(200).json({
+      success: true,
+      text: "il like è presente",
+    });
+  } else {
+    res.status(200).json({
+      succes: false,
+      text: "il like non è presente",
+    });
+  }
 });
 
 likesRouter.post("/getCounter", (req, res) => {
-    const { postId } = req.body;
+  const { postId } = req.body;
 
-    const totalLikes = Object.values(likes);
-    const flatTotalLikes = totalLikes.flat();
+  const totalLikes = Object.values(likes);
+  const flatTotalLikes = totalLikes.flat();
 
-    const filterTotalLikes = flatTotalLikes.filter((el) => el === postId);
-    const counter = filterTotalLikes.length;
+  const filterTotalLikes = flatTotalLikes.filter((el) => el === postId);
+  const counter = filterTotalLikes.length;
 
-    res.status(200).json({
-        success: true,
-        counter: counter,
-    });
+  res.status(200).json({
+    success: true,
+    counter: counter,
+  });
+});
+
+likesRouter.post("/userLikeIt", (req, res) => {
+  const { postId } = req.body;
+  const keys = Object.keys(likes);
+  const usersLikeIt = [];
+  keys.forEach(
+    (el) => likes[el].find((el) => el === postId) && usersLikeIt.push(el)
+  );
+
+  res.status(200).json({ success: true, usersLikeIt: usersLikeIt });
 });
--- to-explore/server/prova.js
+++
@@ -1,8 +0,0 @@
-const ogg = {
-  color: "red",
-  id: 14,
-};
-
-ogg.color = "yellow";
-
-console.log(ogg);
